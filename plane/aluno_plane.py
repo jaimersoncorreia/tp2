54f859fce5db33ca7f4fae12c39504e3490a5fcd
Jaimerson Correia dos Santos <jaimerson@localhost.localdomain>
2017-05-09 21:41:46 -0400

    Inicio do trabalh tp2

---
+++ plane/aluno_plane.py
@@ -0,0 +1,57 @@
+from math import sin, cos, pi, sqrt
+from OpenGL.GL import *
+
+# Aqui voce deve definir o corpo desta funcao de modo a incluir as
+# transofmacoes, repeticoes, condicionais, etc. necessarias para concluir
+# os exercicios.
+#
+# Pequena referencia rapida das funcoes mais usadas:
+#
+# - Translacao: glTranslate(dx, dy, dz)
+#
+# - Rotacao: glRotate(angle, axis_x, axis_y, axis_z)
+#
+# - Escala: glScale(sx, sy, sz)
+#
+# - Espelhamento: Usar escala com fator de -1.0 no eixo em relacao ao qual a
+#   escala deve ocorrer
+#
+# - Salvar transformacao atual: glPushMatrix()
+#
+# - Recuperar ultima transformacao salva: glPopMatrix()
+
+
+def compor_cena(c):
+    c.draw("plane")
+
+
+def compor_cena_ex1(c):
+    c.draw("plane")
+
+
+def compor_cena_ex2(c):
+    c.draw("plane")
+
+
+def compor_cena_ex3(c):
+    c.draw("plane")
+
+
+def compor_cena_ex4(c):
+    c.draw("plane")
+
+
+def compor_cena_ex5(c):
+    c.draw("plane")
+
+
+def compor_cena_ex6(c):
+    c.draw("plane")
+
+
+def compor_cena_ex7(c):
+    c.draw("plane")
+
+
+def compor_cena_ex8(c):
+    c.draw("plane")
